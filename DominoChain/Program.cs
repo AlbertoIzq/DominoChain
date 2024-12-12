59d6a5a5083fc3660ee061b6ea9ad5e36be67608
Alberto <alberto.izquierdomo@gmail.com>
2024-12-12 15:23:45 +0100

    Read and display the domino set

--- DominoChain/Program.cs
+++ DominoChain/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DominoChain;
+
+try
+{
+    Console.WriteLine("Domino chain solver app\n");
+
+    Console.WriteLine("Introduce the set of dominos, one by one like \"1 2\"" +
+        "and press enter after each one. When you have finished, write \"end\":");
+
+    var dominos = new List<(int, int)>();
+
+    string input = Console.ReadLine();
+
+    while (!input.Equals("end"))
+    {
+        dominos.Add(Utilities.ParseToTuple(input));
+        input = Console.ReadLine();
+    }
+
+    Console.WriteLine("");
+    Console.Write("Your domino set is:");
+
+    for (int i = 0; i < dominos.Count; i++)
+    {
+        Console.Write(" ");
+        Console.Write($"[{dominos[i].Item1} {dominos[i].Item2}]");
+    }
+
+    Console.WriteLine("\n");
+
+    // Try to find a circular chain
+    var chain = Utilities.FindCircularChain(dominos);
+
+    if (chain != null)
+    {
+        Console.WriteLine("The found circular domino chain is:");
+        foreach (var domino in chain)
+        {
+            Console.Write($"[{domino.Item1}|{domino.Item2}] ");
+        }
+    }
+    else
+    {
+        Console.WriteLine("A circular domino chain is not possible.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"\nException: {ex.Message}");
+}
---
+++ DominoChain/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoChain
+{
+    public class Utilities
+    {
+        public static (int, int) ParseToTuple(string input)
+        {
+            // Split the input string by space and convert to integers
+            var parts = input.Split(' ');
+
+            // Parse each part to integer and return as a tuple
+            if (parts.Length == 2)
+            {
+                return (int.Parse(parts[0]), int.Parse(parts[1]));
+            }
+            else
+            {
+                throw new ArgumentException("Input string must contain exactly two integers separated by a space.");
+            }
+        }
+
+        // Method to find circular chain which can be considered as an Eulerian Circuit
+        public static List<(int, int)> FindCircularChain(List<(int, int)> dominos)
+        {
+            return null;
+        }
+}
